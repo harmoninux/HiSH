7f66c05b991360d5bab68756bf61f308259623ae
aiminick <aiminick@qq.com>
2026-01-18 20:43:45 +0800

    qemu build tunning

---
+++ deps/libattr/xattr.h
@@ -0,0 +1,10 @@
+/*
+ * Shim header: attr/xattr.h -> sys/xattr.h
+ * Modern attr library deprecated attr/xattr.h in favor of sys/xattr.h
+ */
+#ifndef _ATTR_XATTR_H
+#define _ATTR_XATTR_H
+
+#include <sys/xattr.h>
+
+#endif /* _ATTR_XATTR_H */
