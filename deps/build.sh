f9498e66ecf363d8d0ffb9501238195034e2dcac
hackeris <hackeris@qq.com>
2026-03-14 18:04:43 +0800

    更新 deps/Makefile

--- deps/build.sh
+++ deps/build.sh
@@ -4,23 +4,6 @@ set -e
 # deveco command line tools is downloaded from:
 # https://developer.huawei.com/consumer/cn/download/
 # and extracted to any dir
-#export TOOL_HOME=""
 
-if [[ ! -n ${TOOL_HOME} ]]; then
-  echo """\$TOOL_HOME IS NOT DEFINED, PLS SPECIFIY A CORRECT DIR!
-  You can download HarmonyOS Commandline Tools form
-  https://developer.huawei.com/consumer/cn/download/
-       """
-  exit 1
-fi
-
-export DEVECO_SDK_HOME=$TOOL_HOME/sdk
-export OHOS_SDK_HOME=$TOOL_HOME/sdk/default/openharmony
-export PATH=$TOOL_HOME/bin:$PATH
-export PATH=$TOOL_HOME/tool/node/bin:$PATH
-# set default to x86_64 for emulator
-# to build for arm64, set OHOS_ARCH=aarch64, OHOS_ABI=arm64-v8a
-export OHOS_ARCH=x86_64
-export OHOS_ABI=x86_64
-
-make
+# use `make aarch64` to build for arm64-v8a
+make x86_64 
